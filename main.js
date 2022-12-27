06c0db70167b3594ddb11e1a5bcf5c994c60374d
hung yu <yuehyu1028@gmail.com>
2022-12-27 16:54:17 +0800

    add

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ window.addEventListener('scroll',function(){
   const header=this.document.querySelector('header');
   header.classList.toggle('sticky',this.window.scrollY>0)
 })
+
+// navigation menu close after clicked any link
+function toggleMenu(){
+  menuToggle.classList.remove('active');
+  navigation.classList.remove('active');
+}
